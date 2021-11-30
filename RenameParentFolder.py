7e3c3f32a4d9ec2d4bb501e0a161e9ab092961bf
Tov Are <tovare13@gmail.com>
2021-11-30 11:56:13 +0100

    Added support for "Folders in folder"

--- RenameParentFolder.py
+++ RenameParentFolder.py
@@ -3,31 +3,44 @@ import exifread
 import re
 
 
+
 Root_Folder = "C:\WORKFOLDER\Rename After first image\Testmappe\Importerte bilder"
 # Root_Folder = input("Root Folder: ")
 File_Extension = ".jpg",".jpeg",".png",".tiff",".JPG",".JPEG",".PNG",".TIFF"
 Folder_List = os.listdir(Root_Folder)
 
-Check_for_aldready_dated_folders_patter = "^\d\d\d\d[-]\d\d[-]\d\d"
+Check_for_aldready_dated_folders_pattern = "^\d\d\d\d[-]\d\d[-]\d\d"
 
 # print(Folder_List) 
 
-def Get_Exif_Date_Taken(Folder_To_Rename, Image_With_Exif):
-    with open(Folder_To_Rename + "\\" + Image_With_Exif, "rb") as image:
+def Get_Exif_Date_Taken(Image_With_Exif):
+    with open(Image_With_Exif, "rb") as image:
         tags = exifread.process_file(image, stop_tag="EXIF DateTimeOriginal")
         dateTaken = tags["EXIF DateTimeOriginal"]
         return dateTaken
 
 
 for folder in Folder_List:
-    if not re.match(Check_for_aldready_dated_folders_patter, folder[:10]):
+    if not re.match(Check_for_aldready_dated_folders_pattern, folder[:10]):
         Folder_To_Rename = Root_Folder + "\\" + folder
-        Image_List = [fi for fi in os.walk(Folder_To_Rename) if fi.endswith(File_Extension)]
-
+        # Image_List = [_ for _ in os.listdir(Folder_To_Rename) if _.endswith(File_Extension)]
+        Image_List = []
+        for item in os.listdir(Folder_To_Rename):
+            if item.endswith(File_Extension):
+                Image_List.append(os.path.join(Folder_To_Rename, item))
+        
+        if len(Image_List) == 0:
+            for root, dirs, files in os.walk(Folder_To_Rename):
+                for item in files: 
+                    if item.endswith(File_Extension):
+                        Image_List.append(os.path.join(root, item))
+        
+        #Det er EXIF delen som trenger full path til bilde.
         if not len(Image_List) == 0:
             Image_With_Exif = Image_List[0]
-            date_Taken = str(Get_Exif_Date_Taken(Folder_To_Rename, Image_With_Exif))[:10].strip().replace(":","-")
+            date_Taken = str(Get_Exif_Date_Taken(Image_With_Exif))[:10].strip().replace(":","-")
             Folder_New_Name = date_Taken + " - " + folder
+            #Trenger ikke tenke på Folder_To_Rename. Denne blir riktig.
             os.rename(Folder_To_Rename, Root_Folder + "\\" + Folder_New_Name)
             print(Folder_New_Name)
         else: pass
